c7fda526125c9994c782f76bceb77bad3df1b12b
Michael Schwartz <MichaelSchwar3@gmail.com>
2018-10-23 12:54:59 -0400

    Configure store and test window methods

--- frontend/fasana.jsx
+++ frontend/fasana.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+//test imports
+import { signup, login, logout } from './actions/session_actions';
+import { fetchRuns } from './actions/run_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Fasana</h1>, root);
+  let store;
+  if (window.currentUser) {
+    const preLoadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { currentUserId: window.currentUser.id }
+    };
+    store = configureStore(preLoadedState);
+  }else {
+    store = configureStore();
+  }
+
+//Beginning of Testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+  window.store = store;
+  window.fetchRuns = fetchRuns
+
+//End of Testing
+  ReactDOM.render(<Root store={store}/>, root);
 });
